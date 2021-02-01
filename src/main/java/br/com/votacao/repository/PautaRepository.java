1320bc50a93659bed4de7d4959869050eacadb97
avoliveira <adriano.vidal@castgroup.com.br>
2021-01-31 22:17:46 -0300

    Incluindo PautaRepository

---
+++ src/main/java/br/com/votacao/repository/PautaRepository.java
@@ -0,0 +1,7 @@
+package br.com.votacao.repository;
+
+import br.com.votacao.domain.Pauta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PautaRepository extends CrudRepository<Pauta, Integer> {
+}
